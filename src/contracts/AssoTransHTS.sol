2c68f27b46d016f77ff40fb1006f72ff0336f95a
Namoj-design <namojperiakumar@gmail.com>
2025-09-14 10:03:52 +0530

    add error free contract

--- src/contracts/AssoTransHTS.sol
+++ src/contracts/AssoTransHTS.sol
@@ -9,13 +9,17 @@ contract AssoTransHTS is HederaTokenService {
 
     address tokenAddress;
 
-    constructor(address _tokenAddress) public {
+    constructor(address _tokenAddress) {
         tokenAddress = _tokenAddress;
-     }
+    }
 
-    function tokenAssoTrans(int64 _amount) external {        
+    function tokenAssoTrans(int64 _amount) external returns (bool) {        
         int response1 = HederaTokenService.associateToken(address(this), tokenAddress);
-    
+        require(response1 == HederaResponseCodes.SUCCESS, "Token association failed");
+
         int response2 = HederaTokenService.transferToken(tokenAddress, msg.sender, address(this), _amount);
+        require(response2 == HederaResponseCodes.SUCCESS, "Token transfer failed");
+
+        return true;
     }
 }
