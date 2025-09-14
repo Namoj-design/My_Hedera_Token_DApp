0be36bd68bf3eab336f287fb5d01a774527e856a
Namoj-design <namojperiakumar@gmail.com>
2025-09-14 09:57:31 +0530

    add new values

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
-function MyButton(props) {
+function MyButton({
+	fcn,
+	buttonLabel,
+	type = "button",
+	disabled = false,
+	className = "",
+	children,
+	...rest
+}) {
+	const combinedClassName = ["cta-button", className].filter(Boolean).join(" ");
 	return (
 		<div>
-			<button onClick={props.fcn} className="cta-button">
-				{props.buttonLabel}
+			<button
+				onClick={fcn}
+				type={type}
+				disabled={disabled}
+				className={combinedClassName}
+				{...rest}
+			>
+				{children ? children : buttonLabel}
 			</button>
 		</div>
 	);
 }
+
 export default MyButton;
